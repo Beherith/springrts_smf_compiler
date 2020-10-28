f04ddd6bea370af22d84def43ac05213084d89fc
Beherith <mysterme@gmail.com>
2020-10-28 09:41:26 +0100

    Delete DragAndDropOnThis_TGA_to_DNTS_DDS_Flip_GInvert.bat

--- DragAndDropOnThis_TGA_to_DNTS_DDS_Flip_GInvert.bat
+++
@@ -1,4 +0,0 @@
-
-ECHO "Drag And drop .TGA files onto this to convert them to DDS (while flipping them and inverting G channel"
-
-nvdxt.exe -dxt5 -Sinc -quality_highest -flip -contrast 1.0 -1.0 1.0 1.0 -file "%~1"
