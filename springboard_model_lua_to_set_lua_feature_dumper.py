fe58ee3880c2c7500c646b8eb3a2d0dfc10fd81d
Beherith <mysterme@gmail.com>
2022-03-24 17:35:19 +0100

    Update springboard_model_lua_to_set_lua_feature_dumper.py

--- springboard_model_lua_to_set_lua_feature_dumper.py
+++ springboard_model_lua_to_set_lua_feature_dumper.py
@@ -4,6 +4,7 @@ outf = open("featureplacement_set.lua","w")
 fdef = None
 fx = None
 fz = None
+fy = None
 def ex(s,k):
 	return s.partition(k)[2].strip().strip(',').strip()
 for line in open('model.lua').readlines():
@@ -12,14 +13,21 @@ for line in open('model.lua').readlines():
 	if ' pos = {' in line:
 		fx = 0
 		fz = 0
-	if fx ==0 or fz == 0:
+	if fx == 0 or fz == 0:
 		if ' x = ' in line:
 			fx = int(float( ex(line," x = ")))
 		if ' z = ' in line:
 			fz = int(float( ex(line," z = ")))
-			o = '{ name = %s, x = %d, z = %d, rot = %d },'%(fdef,fx,fz,randint(-32000,32000))
+	if ' rot = {' in line:
+		fy = 0
+	if fy == 0:
+		if ' y = ' in line:
+			fy = float(float(ex(line," y = ")))
+			fy = int(fy*-10185.925)
+			o = '{ name = %s, x = %d, z = %d, rot = %d },'%(fdef,fx,fz,fy)
 			print (o)
 			outf.write(o+'\n')
 			name = None
 			fx = None
 			fz = None
+			fy = None
