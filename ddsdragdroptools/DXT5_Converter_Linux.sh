a15551a57a61ef159e9c3a86895aeb7fc046a1f4
Moose <lwagner5465@gmail.com>
2022-05-09 12:09:31 -0700

    Added chmods

--- ddsdragdroptools/DXT5_Converter_Linux.sh
+++ ddsdragdroptools/DXT5_Converter_Linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##todo chmods
+chmod +x dragon-dxt5 magick nvtt_export.exe
 ./dragon-dxt5 --target | while read -r input 
 do
     ##  ./"nvtt_export.exe" --output "$(echo "$input"|sed -r "s/.+\/(.+)\..+/\1/").dds" --save-flip-y --mip-filter 0 --quality 3 --format bc3 "$(echo "$input"|sed -e 's/%20/ /g;s/^.......//g;s/^/z:/g')"
