bb974aa6b5880a9216cb7b49ac75adf9669ec460
Eugene 'marengohue' Rebedailo <marengohue@protonmail.com>
2023-04-15 19:58:54 +0200

    Fix windows paths

--- build/win/build.bat
+++ build/win/build.bat
@@ -1,10 +1,10 @@
 python3 -m PyInstaller ^
     --noconfirm --onefile --log-level=WARN --noupx ^
     --icon icon.ico ^
-    --paths '..\\..\\src' ^
-    --distpath ..\\..\\bin ^
-    --add-data '..\\..\\resources/*:resources' ^
-    --add-data '..\\..\\LICENSE:.' ^
+    --paths '..\..\src' ^
+    --distpath ..\..\bin ^
+    --add-data '..\..\resources/*:resources' ^
+    --add-data '..\..\LICENSE:.' ^
     --add-binary 'nvdxt.exe:.' ^
     --noconfirm ^
     '..\\..\\src\\pymapconv.py'
