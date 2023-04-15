84a547f15d61f51dcac76a31facfd2ca2ef9d997
Eugene 'marengohue' Rebedailo <marengohue@protonmail.com>
2023-04-15 23:20:40 +0200

    try me

--- build/win/build.ps1
+++ build/win/build.ps1
@@ -1,11 +1,10 @@
-ls
 python3 -m PyInstaller `
     --noconfirm --onefile --log-level=WARN --noupx `
     --icon icon.ico `
     --paths '../../src' `
     --distpath '../../bin' `
-    --add-data '../../resources/*:resources' `
-    --add-data '../../LICENSE:.' `
-    --add-binary 'nvdxt.exe:.' `
+    --add-data '../../resources/*;resources' `
+    --add-data '../../LICENSE;.' `
+    --add-binary 'nvdxt.exe;.' `
     --noconfirm `
     '../../src/pymapconv.py'
